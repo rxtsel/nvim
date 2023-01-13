ed2d09416716260a713181587e834df7d0c7d28b
Cristhian Melo <rxtsel@hotmail.com>
2023-01-13 12:15:09 -0500

    fix: bug fix to lspsaga

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -1,16 +1,18 @@
 local status, saga = pcall(require, "lspsaga")
 if (not status) then return end
 
-saga.init_lsp_saga {
+saga.setup {
   server_filetype_map = {
     typescript = "typescript"
   }
 }
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<C-}>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<Space>i', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
-vim.keymap.set('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
+vim.keymap.set({ 'n', 't' }, '<A-d>', '<cmd>Lspsaga term_toggle<CR>', opts)
+vim.keymap.set('n', 'gh', '<cmd>Lspsaga lsp_finder<CR>', opts)
