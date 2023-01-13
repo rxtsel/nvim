d894827d95c6edcb169c96c22dff05aac6bbe27c
Cristhian Melo <rxtsel@hotmail.com>
2023-01-13 18:37:58 -0500

    chore: add more maps

--- lua/maps.lua
+++ lua/maps.lua
@@ -2,16 +2,16 @@ local keymap = vim.keymap
 
 keymap.set('n', 'x', '"_x')
 
--- save and exit
+-- Save and exit
 keymap.set('n', '<Space>q', ':q<CR>')
 keymap.set('n', '<Space>w', ':w<CR>')
 keymap.set('n', '<A-q>', ':bd!<CR>')
 
--- comma to end
+-- Comma to end
 keymap.set("n", "<Space>;", "$a;<Esc>")
 keymap.set("n", "<Space>,", "$a,<Esc>")
 
--- move lines
+-- Move lines
 keymap.set("n", "<A-j>", ":m .+1<CR>==")
 keymap.set("n", "<A-k>", ":m .-2<CR>==")
 keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
@@ -19,10 +19,10 @@ keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
 keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
 keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 
--- nvim-tree
+-- Nvim-tree
 keymap.set("n", "<Space>e", ":NvimTreeToggle<CR>")
 
--- open term
+-- Open term
 keymap.set('t', '<Esc>', '<C-\\><C-n>', { silent = true })
 
 -- Increment/decrement
@@ -53,8 +53,15 @@ keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
 
--- hop
+-- Hop
 keymap.set('n', '<Space>s', ':HopChar2<CR>')
 
--- greatest remap ever
+-- Greatest remap ever
 keymap.set('x', '<Space>p', "\"_dP")
+
+-- Indentations
+keymap.set("v", ">", ">gv", { silent = true })
+keymap.set("v", "<", "<gv", { silent = true })
+
+-- Clear search highlight
+keymap.set("n", "<esc>", ":noh<return><esc>", { noremap = true, silent = true })
