d47da799104224d7ccdd13d1f328c18eeb26299f
Cristhian Melo <rxtsel@hotmail.com>
2023-01-13 18:14:53 -0500

    fix: add support to js

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -72,7 +72,7 @@ nvim_lsp.flow.setup {
 
 nvim_lsp.tsserver.setup {
   on_attach = on_attach,
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
   cmd = { "typescript-language-server", "--stdio" },
   capabilities = capabilities
 }
