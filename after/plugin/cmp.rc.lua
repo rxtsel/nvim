58767e364761b1d5c5de35e1a45b039b07fcda85
Cristhian Melo <rxtsel@hotmail.com>
2023-01-14 14:40:42 -0500

    chore: add maps for move in cmp suggestions

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -3,17 +3,6 @@ if (not status) then return end
 
 local lspkind = require 'lspkind'
 
-local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
-local has_words_before = function()
-  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0
-      and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s")
-      == nil
-end
-
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -21,32 +10,27 @@ cmp.setup({
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
+    ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
-      select = true
-    }),
-    ["<c-j>"] = cmp.mapping(function(fallback)
+      select = true,
+    },
+    ['<C-j>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif vim.fn["luasnip#available"]() == 1 then
-        feedkey("<Plug>(luasnip-expand-or-jump)", "")
-      elseif has_words_before() then
-        cmp.complete()
       else
-        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        fallback()
       end
-    end, { "i", "s" }),
-    ["<c-k>"] = cmp.mapping(function()
+    end, { 'i', 's' }),
+    ['<C-k>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif vim.fn["luasnip#jumpable"](-1) == 1 then
-        feedkey("<Plug>(luasnip-jump-prev)", "")
+      else
+        fallback()
       end
-    end, { "i", "s" })
+    end, { 'i', 's' }),
   }),
   sources = cmp.config.sources({
     { name = 'path' },
