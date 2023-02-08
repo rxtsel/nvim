f1adc5297ede0fcb18dcd8432094897806c935a4
Cristhian Melo <rxtsel@hotmail.com>
2023-02-08 16:10:35 -0500

    chore: add line spaces

--- lua/maps.lua
+++ lua/maps.lua
@@ -41,9 +41,11 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- New tab
 keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
+
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
+
 -- Move window
 keymap.set('n', '<Space>', '<C-w>w')
 keymap.set('', 'sh', '<C-w>h')
