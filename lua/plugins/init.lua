d009fc71c9ecb52bf83497790f410628bf799f06
Cristhian Melo <rxtsel@hotmail.com>
2023-03-30 01:43:10 -0500

    feat: migrate to lazy

---
+++ lua/plugins/init.lua
@@ -0,0 +1,75 @@
+return {
+  'glepnir/lspsaga.nvim',  -- LSP UIs
+  'onsails/lspkind-nvim',  -- vscode-like pictograms
+  'folke/lsp-colors.nvim', -- LSP diagnostics colors
+  {
+    "L3MON4D3/LuaSnip",
+    version = "1.2.1",
+    build = "make install_jsregexp"
+  },
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-buffer', -- nvim-cmp source for buffer words
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp'
+    }
+  },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim'
+    }
+  },
+  'b0o/schemastore.nvim', -- json schemas to use with lspconfig
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  },
+  'jose-elias-alvarez/null-ls.nvim', -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  'MunifTanjim/prettier.nvim',       -- Prettier plugin for Neovim',s built-in LSP client
+  'kyazdani42/nvim-tree.lua',
+
+  {
+    'numToStr/Comment.nvim',
+    requires = {
+      'JoosepAlviste/nvim-ts-context-commentstring'
+    }
+  },
+
+  'windwp/nvim-autopairs',
+  'windwp/nvim-ts-autotag',
+
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+    }
+  },
+
+  'akinsho/nvim-bufferline.lua',
+
+  'norcalli/nvim-colorizer.lua',
+
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  },
+  'dinhhuy258/git.nvim', -- For git blame & browse
+
+  'phaazon/hop.nvim',
+
+  'github/copilot.vim',
+
+  'terryma/vim-multiple-cursors',
+
+  'andweeb/presence.nvim',
+}
--- lua/rxtsel/plugins.lua
+++ lua/rxtsel/plugins.lua
@@ -1,80 +1,14 @@
-local status, packer = pcall(require, "packer")
-if (not status) then
-  print("Packer is not installed")
-  return
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd [[packadd packer.nvim]]
-
-packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {
-    'svrana/neosolarized.nvim',
-    requires = { 'tjdevries/colorbuddy.nvim' }
-  }
-  use 'kyazdani42/nvim-web-devicons' -- File icons
-  use 'glepnir/lspsaga.nvim'         -- LSP UIs
-  use 'nvim-lualine/lualine.nvim'    -- Statusline
-  use 'onsails/lspkind-nvim'         -- vscode-like pictograms
-  use 'folke/lsp-colors.nvim'        -- LSP diagnostics colors
-  use({
-    "L3MON4D3/LuaSnip",
-    tag = "1.2.1",
-    run = "make install_jsregexp"
-  })
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-buffer'    -- nvim-cmp source for buffer words
-  use 'hrsh7th/cmp-nvim-lsp'  -- nvim-cmp source for neovim's built-in LSP
-  use 'hrsh7th/nvim-cmp'      -- Completion
-  use 'neovim/nvim-lspconfig' -- LSP
-  use 'b0o/schemastore.nvim'  -- json schemas to use with lspconfig
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-
-  }
-  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-  use 'MunifTanjim/prettier.nvim'       -- Prettier plugin for Neovim's built-in LSP client
-  use 'kyazdani42/nvim-tree.lua'
-
-  use { 'numToStr/Comment.nvim',
-    requires = {
-      'JoosepAlviste/nvim-ts-context-commentstring'
-    }
-  }
-
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
-
-  use 'nvim-lua/plenary.nvim' -- Common utilities
-  use 'nvim-telescope/telescope.nvim'
-
-  use 'akinsho/nvim-bufferline.lua'
-
-  use 'norcalli/nvim-colorizer.lua'
-
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-
-  use({
-    "iamcco/markdown-preview.nvim",
-    run = function() vim.fn["mkdp#util#install"]() end,
-  })
-
-  use {
-    'lewis6991/gitsigns.nvim',
-    config = function()
-      require('gitsigns').setup()
-    end
-  }
-  use 'dinhhuy258/git.nvim' -- For git blame & browse
-
-  use 'phaazon/hop.nvim'
-
-  use 'github/copilot.vim'
-
-  use 'terryma/vim-multiple-cursors'
-
-  -- presense discord
-  use 'andweeb/presence.nvim'
-end)
+require('lazy').setup('plugins')
