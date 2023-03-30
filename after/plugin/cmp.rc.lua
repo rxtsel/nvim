50d3c52652b2ccad97e50662dd85a0a52d7813ae
Cristhian Melo <rxtsel@hotmail.com>
2023-03-30 13:05:40 -0500

    fix: bug fix load copilot

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -44,7 +44,7 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true
     }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<C-j>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -55,7 +55,7 @@ cmp.setup({
         fallback()
       end
     end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    ["<C-k>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
