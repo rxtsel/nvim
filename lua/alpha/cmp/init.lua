fb0e4765d10e3806520b7ec9f6ef060c05e00429
Cristhian Melo <rxtsel@hotmail.com>
2023-03-30 21:19:37 -0500

    chore: migrate cmp to lazy

---
+++ lua/alpha/cmp/init.lua
@@ -0,0 +1,92 @@
+local cmp = require "cmp"
+local lspkind = require "lspkind"
+local luasnip = require "luasnip"
+local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+local compare = require "cmp.config.compare"
+
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
+
+cmp.setup {
+  mapping = {
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-e>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.close()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<C-n>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.choice_active() then
+        luasnip.change_choice(1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+    ["<c-space>"] = cmp.mapping.complete(),
+  },
+  sources = {
+    { name = "path" },
+    { name = "luasnip" },
+    { name = "nvim_lsp" },
+    {
+      name = "buffer",
+      keyword_length = 4,
+      option = {
+        get_bufnrs = function()
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local bufnr = vim.api.nvim_win_get_buf(win)
+            if vim.api.nvim_buf_get_option(bufnr, 'buftype') ~= 'terminal' then
+              bufs[bufnr] = true
+            end
+          end
+          return vim.tbl_keys(bufs)
+        end,
+      },
+    },
+  },
+
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+
+  formatting = {
+    format = lspkind.cmp_format {
+      with_text = false,
+      maxwidth = 50,
+      menu = {
+        buffer = "[buf]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[api]",
+        path = "[path]",
+        luasnip = "[snip]",
+      },
+    },
+  },
+
+  sorting = {
+    priority_weight = 2,
+    comparators = {
+      compare.kind,
+      compare.sort_text,
+    },
+  },
+
+  experimental = {
+    native_menu = false,
+    ghost_text = false,
+  },
+}
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,10 +6,15 @@ return {
     'hrsh7th/cmp-path',
     'windwp/nvim-autopairs',
     'onsails/lspkind-nvim',
+    'saadparwaiz1/cmp_luasnip',
     {
       "L3MON4D3/LuaSnip",
       version = "v1.*",
       build = "make install_jsregexp"
     }
-  }
+  },
+  event = "VeryLazy",
+  config = function()
+    require "alpha.cmp"
+  end
 }
