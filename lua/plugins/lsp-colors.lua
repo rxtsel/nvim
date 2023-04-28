39285c6ca49ba97f0fce17cbde9b206e77f5889b
Cristhian Melo <rxtsel@hotmail.com>
2023-04-28 11:42:24 -0500

    chore: migrate lsp-color and lspkind config to lazy

---
+++ lua/plugins/lsp-colors.lua
@@ -0,0 +1,9 @@
+return {
+  'folke/lsp-colors.nvim',
+  config = {
+    Error = "#db4b4b",
+    Warning = "#e0af68",
+    Information = "#0db9d7",
+    Hint = "#10B981"
+  }
+}
---
+++ lua/plugins/lspkind.lua
@@ -0,0 +1,34 @@
+return {
+  'onsails/lspkind-nvim',
+  lazy = {
+    mode = 'symbol',
+    preset = 'codicons',
+    symbol_map = {
+      Text = "",
+      Method = "",
+      Function = "",
+      Constructor = "",
+      Field = "ﰠ",
+      Variable = "",
+      Class = "ﴯ",
+      Interface = "",
+      Module = "",
+      Property = "ﰠ",
+      Unit = "塞",
+      Value = "",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      Constant = "",
+      Struct = "פּ",
+      Event = "",
+      Operator = "",
+      TypeParameter = ""
+    }
+  }
+}
