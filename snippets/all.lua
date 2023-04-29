ff5d8ed83a3e4630f612e844498475957a32fca3
Cristhian Melo <rxtsel@hotmail.com>
2023-04-29 17:35:40 -0500

    chore: remove snippets

--- snippets/all.lua
+++ snippets/all.lua
@@ -5,9 +5,5 @@ local t = ls.text_node
 return {
   s("copyright", t("&#169;")),
 
-  s("all_rights_reserved", t("All rights reserved.")),
-
-  s("powered_by", t("Powered by")),
-
-  s("powered_by_link", t("https://rxtsel.dev"))
+  s("rxtsel_website", t("https://rxtsel.dev"))
 }
