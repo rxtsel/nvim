7e9fe1cc61cee054aac0f56482a50cfdd60b7c20
Cristhian Melo <rxtsel@hotmail.com>
2023-05-01 14:30:31 -0500

    chore: reordering cmp suggestions

--- lua/alpha/cmp/init.lua
+++ lua/alpha/cmp/init.lua
@@ -21,28 +21,6 @@ cmp.setup {
       select = true,
     },
     ["<c-space>"] = cmp.mapping.complete(),
-    -- start tabs for snippets
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    -- end tabs for snippets
     ["<C-n>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -107,11 +85,11 @@ cmp.setup {
       with_text = false,
       maxwidth = 50,
       menu = {
-        buffer = "[buf]",
+        luasnip = "[snip]",
         nvim_lsp = "[LSP]",
+        buffer = "[buf]",
         nvim_lua = "[api]",
         path = "[path]",
-        luasnip = "[snip]",
       },
     },
   },
