f902fa43ee25ddfb9740cfb7d4b7df9c6c75421d
Cristhian Melo <rxtsel@hotmail.com>
2023-05-21 15:46:49 -0500

    feat: add icon for astro files

--- lua/plugins/web-devicons.lua
+++ lua/plugins/web-devicons.lua
@@ -2,5 +2,12 @@ return {
   'kyazdani42/nvim-web-devicons',
   config = {
     default = true,
+    override = {
+      astro = {
+        icon = '',
+        color = '#FF5D01',
+        name = 'Astro'
+      }
+    }
   }
 }
