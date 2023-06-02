0711fb779c1f95e2a1813d93a78e594297b08f9b
Cristhian Melo <rxtsel@hotmail.com>
2023-06-01 21:54:19 -0500

    chore: remove unuse plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,21 +1,9 @@
 return {
   'nvim-lua/plenary.nvim',
 
-  {
-      'williamboman/mason.nvim',
-      build = ':MasonUpdate'
-    },
-
-    'williamboman/mason-lspconfig.nvim',
-
-  -- 'numToStr/Comment.nvim', -- potentially remove this...
-  'tpope/vim-rhubarb',
-  'prisma/vim-prisma',
-  'tpope/vim-sleuth',
-
   -- comments
   { 'JoosepAlviste/nvim-ts-context-commentstring', lazy = true },
 
   -- ui components
-  { 'MunifTanjim/nui.nvim', lazy = true },
+  { 'MunifTanjim/nui.nvim',                        lazy = true },
 }
