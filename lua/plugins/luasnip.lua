3d6f124074aafcec4d8df0e8a7e6be344be4cce3
Cristhian Melo <rxtsel@hotmail.com>
2023-06-22 16:20:19 -0500

    fix: remove duplicate code

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,6 +4,5 @@ return {
   build = "make install_jsregexp",
   config = function()
     require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
-    require('luasnip').filetype_extend("javascript", { "javascriptreact" })
   end
 }
