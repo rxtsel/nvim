4a8314be695b9c2002a17957223feaa177b83453
Cristhian Melo <rxtsel@hotmail.com>
2023-07-04 15:03:17 -0500

    chore: add snippet to ts-react

--- snippets/typescriptreact.lua
+++ snippets/typescriptreact.lua
@@ -1,3 +1,24 @@
 local ls = require("luasnip")
 ls.filetype_extend("typescriptreact", { "javascript" })
 ls.filetype_extend("typescriptreact", { "javascriptreact" })
+
+return {
+  -- arrow function component
+  ls.parser.parse_snippet({
+      trig = "rafce"
+    },
+    [[
+    import { FC } from 'react'
+
+    export interface ${1: nameFunction}Props {${2}}
+
+    export const ${1: nameFunction}:FC<${1: nameFunction}Props> = () => {
+      return (
+        ${3:<></>}
+      )
+    }
+
+    export default ${1: nameFunction}
+    ]]
+  )
+}
