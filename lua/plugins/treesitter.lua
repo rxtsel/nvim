6e9af773042e16add3549ac5dd92f8cb5115bc95
Cristhian Melo <rxtsel@hotmail.com>
2023-08-18 16:31:35 -0500

    fix: fix comment-ts-context-string

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -31,7 +31,11 @@ return {
       },
       autotag = {
         enable = true,
-      }
+      },
+      context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+      },
     }
 
     local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
