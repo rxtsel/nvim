9cbdb36aa011486fa835fdc28cc77a266cdf9afa
Cristhian Melo <rxtsel@hotmail.com>
2023-08-18 14:48:45 -0500

    chore: remove has sistem

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
-require('config.base')
-require('config.lazy')
-require('plugins.lsp')
+require 'config.base'
+require 'config.lazy'
+require 'plugins.lsp'
 
 -- lazy.nvim will call the User autocommand after lazy has loaded
 vim.api.nvim_create_autocmd('User', {
@@ -10,18 +10,3 @@ vim.api.nvim_create_autocmd('User', {
     require 'config.maps'
   end,
 })
-
-local has = vim.fn.has
-local is_mac = has "macunix"
-local is_win = has "win32"
-local is_wsl = has "wsl"
-
-if is_mac then
-  require('config.macos')
-end
-if is_win then
-  require('config.windows')
-end
-if is_wsl then
-  require('config.wsl')
-end
