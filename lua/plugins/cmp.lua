44c4cf52bbf07fab8f6a35e5ccab053f2bcd1863
Cristhian Melo <rxtsel@hotmail.com>
2023-08-18 18:02:50 -0500

    chore: add tailwind support

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,5 +1,4 @@
 local M = {
-  -- Autocompletion
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
@@ -8,12 +7,30 @@ local M = {
     'saadparwaiz1/cmp_luasnip',
     'windwp/nvim-autopairs',
     'hrsh7th/cmp-nvim-lsp',
-    -- 'hrsh7th/cmp-path',
     'onsails/lspkind-nvim',
   },
   opts = function()
     local cmp = require('cmp')
     local luasnip = require('luasnip')
+    local lspkind = require('lspkind')
+
+    local function formatForTailwindCSS(entry, vim_item)
+      if vim_item.kind == 'Color' and entry.completion_item.documentation then
+        local _, _, r, g, b = string.find(entry.completion_item.documentation, '^rgb%((%d+), (%d+), (%d+)')
+        if r then
+          local color = string.format('%02x', r) .. string.format('%02x', g) .. string.format('%02x', b)
+          local group = 'Tw_' .. color
+          if vim.fn.hlID(group) < 1 then
+            vim.api.nvim_set_hl(0, group, { fg = '#' .. color })
+          end
+          vim_item.kind = "●"
+          vim_item.kind_hl_group = group
+          return vim_item
+        end
+      end
+      vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
+      return vim_item
+    end
 
     local has_words_before = function()
       unpack = unpack or table.unpack
@@ -57,23 +74,18 @@ local M = {
         end, { 'i', 's' }),
       }),
       sources = {
-        -- { name = 'path' },
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
       },
       formatting = {
-        format = function(_, item)
-          local icons = require('config').icons.kinds
-          if icons[item.kind] then
-            item.kind = icons[item.kind] .. item.kind
+        format = lspkind.cmp_format({
+          maxwidth = 50,
+          before = function(entry, vim_item)
+            vim_item = formatForTailwindCSS(entry, vim_item)
+            return vim_item
           end
-          return item
-        end,
-      },
-      experimental = {
-        native_menu = false,
-        ghost_text = false,
-      },
+        }),
+      }
     }
   end,
   config = function(_, opts)
