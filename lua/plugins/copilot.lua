f10448fb314d894f1e08542f3a2f14eff0d1d9d9
Cristhian Melo <rxtsel@hotmail.com>
2023-08-18 18:26:39 -0500

    chore: change copilot.vim with copilot.lua

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,20 @@
 return {
-  'github/copilot.vim'
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<Tab>",
+          next = "<C-]>",
+          prev = "<C-[>",
+          dismiss = "<Esc>"
+        }
+      }
+    })
+  end,
 }
