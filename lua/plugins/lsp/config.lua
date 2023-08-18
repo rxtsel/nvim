4693bc8884ae66965466bab3cd6725f207901aae
Cristhian Melo <rxtsel@hotmail.com>
2023-08-18 15:54:50 -0500

    feat: add lspsaga

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -1,30 +1,5 @@
 local M = {}
 
-function M.set_keybinds(buf)
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = buf, desc = desc })
-  end
-
-  nmap('<Space>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<Space>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  -- Jumps
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('gD', vim.lsp.buf.type_definition, '[G]oto Type [D]efinition')
-  nmap('<C-j>', vim.diagnostic.goto_next, 'Next diagnostics')
-  nmap('<C-k>', vim.diagnostic.goto_prev, 'Prev diagnostics')
-  nmap('<Space>i', vim.diagnostic.open_float, 'Open float diagnostics')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-end
-
 function M.set_diagnostics()
   -- diagnostics settings
   local opts = {
