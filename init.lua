d880d8ba5070457ebf793ed598b4cda8450757b3
Cristhian Melo <rxtsel@hotmail.com>
2023-08-18 16:46:23 -0500

    chore: remove comment

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ require 'config.base'
 require 'config.lazy'
 require 'plugins.lsp'
 
--- lazy.nvim will call the User autocommand after lazy has loaded
 vim.api.nvim_create_autocmd('User', {
   pattern = 'VeryLazy',
   callback = function()
