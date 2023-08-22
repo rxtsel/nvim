44a275868eac5a9c443ce29a3321eb38d615854c
Cristhian Melo <rxtsel@hotmail.com>
2023-08-22 11:19:34 -0500

    chore: add format with stylua

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
-require 'config.base'
-require 'config.lazy'
-require 'plugins.lsp'
+require("config.base")
+require("config.lazy")
+require("plugins.lsp")
 
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'VeryLazy',
-  callback = function()
-    require 'config.autocmds'
-    require 'config.maps'
-  end,
+vim.api.nvim_create_autocmd("User", {
+	pattern = "VeryLazy",
+	callback = function()
+		require("config.autocmds")
+		require("config.maps")
+	end,
 })
