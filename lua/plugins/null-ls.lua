6925b8703fa0192eb89255fbf0fa7841b25b3147
Cristhian Melo <rxtsel@hotmail.com>
2023-08-22 11:17:55 -0500

    fix: bug fix format on save with null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,27 +1,48 @@
 local M = {
-  'jose-elias-alvarez/null-ls.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  lazy = true,
-  dependencies = {
-    'williamboman/mason.nvim',
-  },
-  opts = function()
-    local null_ls = require('null-ls')
-    local formatting = null_ls.builtins.formatting
-    return {
-      root_dir = require('null-ls.utils').root_pattern('stylua.toml', '.git'),
-      sources = {
-        formatting.prettierd,
-        null_ls.builtins.diagnostics.eslint_d.with({
-          diagnostics_format = '[eslint] #{m}\n(#{c})'
-        })
-      },
-      debug = true,
-    }
-  end,
-  config = function(_, opts)
-    require('null-ls').setup(opts)
-  end,
+	"jose-elias-alvarez/null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"williamboman/mason.nvim",
+	},
+	opts = function()
+		local null_ls = require("null-ls")
+		local formatting = null_ls.builtins.formatting
+		local diagnostics = null_ls.builtins.diagnostics
+
+		return {
+			root_dir = require("null-ls.utils").root_pattern("stylua.toml", ".git"),
+			sources = {
+				formatting.prettierd,
+				formatting.stylua,
+				formatting.eslint_d,
+				diagnostics.eslint_d.with({
+					diagnostics_format = "[eslint] #{m}\n(#{c})",
+				}),
+			},
+			on_attach = function(client, bufnr)
+				-- Enable formatting on sync
+				if client.supports_method("textDocument/formatting") then
+					local format_on_save = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = format_on_save,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({
+								bufnr = bufnr,
+								filter = function(_client)
+									return _client.name == "null-ls"
+								end,
+							})
+						end,
+					})
+				end
+			end,
+			debug = true,
+		}
+	end,
+	config = function(_, opts)
+		require("null-ls").setup(opts)
+	end,
 }
 
 return M
