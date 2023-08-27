21d2a87e85b3e67b8b996c0104b391c462e4d282
Cristhian Melo <rxtsel@hotmail.com>
2023-08-27 10:40:18 -0500

    fix: bug fix tabs in insert mode, change tab with altgr l

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,20 +1,20 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = "<Tab>",
-          next = "<C-]>",
-          prev = "<C-[>",
-          dismiss = "<Esc>"
-        }
-      }
-    })
-  end,
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				debounce = 75,
+				keymap = {
+					accept = "<M-l>",
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<Esc>",
+				},
+			},
+		})
+	end,
 }
