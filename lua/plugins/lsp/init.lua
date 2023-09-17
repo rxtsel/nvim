89b6b5ca86fc099898f89dea6948a960b0a9d933
Cristhian Melo <rxtsel@hotmail.com>
2023-09-17 16:51:08 -0500

    chore: update config

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,65 +1,66 @@
 local M = {
-  -- LSP Configuration & Plugins
-  'neovim/nvim-lspconfig',
-  priority = 1000,
-  dependencies = {
-    -- Automatically install LSPs to stdpath for neovim
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-  },
-  opts = {
-    servers = {
-      astro = {},
-      tailwindcss = {},
-      cssls = {},
-      tsserver = {},
-      jsonls = {},
-      yamlls = {},
-      lua_ls = {
-        Lua = {
-          workspace = { checkThirdParty = false },
-          telemetry = { enable = false },
-        },
-      },
-    },
-  },
-  config = function(_, opts)
-    local config = require('plugins.lsp.config')
-    local servers = opts.servers
+	-- LSP Configuration & Plugins
+	"neovim/nvim-lspconfig",
+	priority = 1000,
+	dependencies = {
+		-- Automatically install LSPs to stdpath for neovim
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	},
+	opts = {
+		servers = {
+			astro = {},
+			tailwindcss = {},
+			cssls = {},
+			tsserver = {},
+			jsonls = {},
+			yamlls = {},
+			lua_ls = {
+				Lua = {
+					workspace = { checkThirdParty = false },
+					telemetry = { enable = false },
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		local config = require("plugins.lsp.config")
+		local servers = opts.servers
 
-    -- Setup diagnostics (including icons)
-    config.set_diagnostics()
+		-- Setup diagnostics (including icons)
+		config.set_diagnostics()
 
-    -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-    local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+		-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+		local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-    -- Setup required for ufo
-    capabilities.textDocument.foldingRange = {
-      dynamicRegistration = false,
-      lineFoldingOnly = true,
-    }
+		-- Setup required for ufo
+		capabilities.textDocument.foldingRange = {
+			dynamicRegistration = false,
+			lineFoldingOnly = true,
+		}
 
-    -- Setup mason so it can manage external tooling
-    require('mason').setup()
+		-- Setup mason so it can manage external tooling
+		require("mason").setup()
 
-    -- Ensure the servers above are installed
-    local mason_lspconfig = require('mason-lspconfig')
+		-- Ensure the servers above are installed
+		local mason_lspconfig = require("mason-lspconfig")
 
-    mason_lspconfig.setup({
-      ensure_installed = vim.tbl_keys(servers),
-    })
+		mason_lspconfig.setup({
+			ensure_installed = vim.tbl_keys(servers),
+		})
 
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        local lsp_config = require('lspconfig')
-        lsp_config[server_name].setup({
-          capabilities = capabilities,
-          on_attach = config.on_attach,
-          settings = servers[server_name],
-        })
-      end,
-    })
-  end,
+		mason_lspconfig.setup_handlers({
+			function(server_name)
+				local lsp_config = require("lspconfig")
+
+				lsp_config[server_name].setup({
+					capabilities = capabilities,
+					on_attach = config.on_attach,
+					settings = servers[server_name],
+				})
+			end,
+		})
+	end,
 }
 
 return M
