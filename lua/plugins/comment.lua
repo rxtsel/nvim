a7be44adde0fe9d7296fbb43d177b85a1c378ff7
Cristhian Melo <rxtsel@hotmail.com>
2023-09-20 09:25:24 -0500

    chore: change config with opts

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -3,7 +3,7 @@ return {
   dependencies = {
     'JoosepAlviste/nvim-ts-context-commentstring'
   },
-  config = {
+  opts = {
     pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
   }
 }
