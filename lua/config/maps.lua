3d22599bd1f99cf0318912ecdf014a16ed5cda49
Cristhian Melo <rxtsel@hotmail.com>
2023-10-27 13:48:44 -0500

    chore: change nvim-tree with neo-tree

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -1,15 +1,15 @@
 local keymap = vim.keymap
 
-keymap.set('n', 'x', '"_x')
+keymap.set("n", "x", '"_x')
 
 -- Save and exit
-keymap.set('n', '<Space>q', ':q<CR>')
-keymap.set('n', '<Space>Q', ':q!<CR>')
-keymap.set('n', '<Space>w', ':w<CR>')
-keymap.set('n', '<A-q>', ':bd!<CR>')
+keymap.set("n", "<Space>q", ":q<CR>")
+keymap.set("n", "<Space>Q", ":q!<CR>")
+keymap.set("n", "<Space>w", ":w<CR>")
+keymap.set("n", "<A-q>", ":bd!<CR>")
 
 -- force format
-keymap.set('n', '<C-s>', ':lua vim.lsp.buf.format()<CR> :w<CR>')
+keymap.set("n", "<C-s>", ":lua vim.lsp.buf.format()<CR> :w<CR>")
 
 -- Comma to end
 keymap.set("n", "<Space>;", "$a;<Esc>")
@@ -23,44 +23,44 @@ keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
 keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
 keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 
--- Nvim-tree
-keymap.set("n", "<Space>e", ":NvimTreeToggle<CR>")
+-- Neo-tree
+keymap.set("n", "<Space>e", ":Neotree toggle<CR>")
 
 -- Increment/decrement
-keymap.set('n', '+', '<C-a>')
-keymap.set('n', '-', '<C-x>')
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
 
 -- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d')
+keymap.set("n", "dw", 'vb"_d')
 
 -- Select all
-keymap.set('n', '<Space>aa', 'gg<S-v>G$')
+keymap.set("n", "<Space>aa", "gg<S-v>G$")
 
 -- New tab
-keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
+keymap.set("n", "te", ":tabedit<Return>", { silent = true })
 
 -- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
+keymap.set("n", "ss", ":split<Return><C-w>w", { silent = true })
+keymap.set("n", "sv", ":vsplit<Return><C-w>w", { silent = true })
 
 -- Move window
-keymap.set('n', '<Space>', '<C-w>w')
-keymap.set('', 'sh', '<C-w>h')
-keymap.set('', 'sk', '<C-w>k')
-keymap.set('', 'sj', '<C-w>j')
-keymap.set('', 'sl', '<C-w>l')
+keymap.set("n", "<Space>", "<C-w>w")
+keymap.set("", "sh", "<C-w>h")
+keymap.set("", "sk", "<C-w>k")
+keymap.set("", "sj", "<C-w>j")
+keymap.set("", "sl", "<C-w>l")
 
 -- Resize window
-keymap.set('n', '<C-w><left>', '<C-w><')
-keymap.set('n', '<C-w><right>', '<C-w>>')
-keymap.set('n', '<C-w><up>', '<C-w>+')
-keymap.set('n', '<C-w><down>', '<C-w>-')
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
 
 -- Hop
-keymap.set('n', '<Space>s', ':HopChar2<CR>')
+keymap.set("n", "<Space>s", ":HopChar2<CR>")
 
 -- Greatest remap ever
-keymap.set('x', '<Space>p', "\"_dP")
+keymap.set("x", "<Space>p", '"_dP')
 
 -- Indentations
 keymap.set("v", ">", ">gv", { silent = true })
