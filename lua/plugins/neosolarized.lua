f9a8b6621fda7dd034b078b46226ca56620fb36d
Cristhian Melo <rxtsel@hotmail.com>
2023-11-02 10:23:56 -0500

    chore: change theme neosolarized with solarized-osaka

--- lua/plugins/neosolarized.lua
+++ lua/plugins/neosolarized.lua
@@ -1,41 +1,36 @@
 return {
-  "svrana/neosolarized.nvim",
-  dependencies = {
-    'tjdevries/colorbuddy.nvim'
-  },
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('neosolarized').setup({ comment_italics = true })
+	"craftzdog/solarized-osaka.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		transparent = true, -- Enable this to disable setting the background color
+		terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+		styles = {
+			-- Style to be applied to different syntax groups
+			-- Value is any valid attr-list value for `:help nvim_set_hl`
+			comments = { italic = true },
+			keywords = { italic = true },
+			functions = {},
+			variables = {},
+			-- Background styles. Can be "dark", "transparent" or "normal"
+			sidebars = "dark", -- style for sidebars, see below
+			floats = "dark", -- style for floating windows
+		},
+		sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+		day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+		hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+		dim_inactive = false, -- dims inactive windows
+		lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
-    local cb = require('colorbuddy.init')
-    local Color = cb.Color
-    local colors = cb.colors
-    local Group = cb.Group
-    local groups = cb.groups
-    local styles = cb.styles
+		--- You can override specific color groups to use other groups or a hex color
+		--- function will be called with a ColorScheme table
+		---@param colors ColorScheme
+		on_colors = function(colors) end,
 
-    Color.new('white', '#ffffff')
-    Color.new('black', '#000000')
-    Group.new('Normal', colors.base1, colors.NONE, styles.NONE)
-    Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
-    Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
-    Group.new('Visual', colors.none, colors.base03, styles.reverse)
-
-    local cError = groups.Error.fg
-    local cInfo = groups.Information.fg
-    local cWarn = groups.Warning.fg
-    local cHint = groups.Hint.fg
-
-    Group.new("DiagnosticVirtualTextError", cError, cError:dark():dark():dark():dark(), styles.NONE)
-    Group.new("DiagnosticVirtualTextInfo", cInfo, cInfo:dark():dark():dark(), styles.NONE)
-    Group.new("DiagnosticVirtualTextWarn", cWarn, cWarn:dark():dark():dark(), styles.NONE)
-    Group.new("DiagnosticVirtualTextHint", cHint, cHint:dark():dark():dark(), styles.NONE)
-    Group.new("DiagnosticUnderlineError", colors.none, colors.none, styles.undercurl, cError)
-    Group.new("DiagnosticUnderlineWarn", colors.none, colors.none, styles.undercurl, cWarn)
-    Group.new("DiagnosticUnderlineInfo", colors.none, colors.none, styles.undercurl, cInfo)
-    Group.new("DiagnosticUnderlineHint", colors.none, colors.none, styles.undercurl, cHint)
-
-    Group.new("HoverBorder", colors.yellow, colors.none, styles.NONE)
-  end
+		--- You can override specific highlights to use other groups or a hex color
+		--- function will be called with a Highlights and ColorScheme table
+		---@param highlights Highlights
+		---@param colors ColorScheme
+		on_highlights = function(highlights, colors) end,
+	},
 }
