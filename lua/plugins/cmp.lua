fbaa133f66f6220dc792baabbe91f03d0c6b1619
Cristhian Melo <rxtsel@hotmail.com>
2023-11-05 13:51:31 -0500

    chore: add path and buffer for cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,99 +1,103 @@
 local M = {
-  'hrsh7th/nvim-cmp',
-  event = 'InsertEnter',
-  dependencies = {
-    'hrsh7th/cmp-nvim-lsp',
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
-    'windwp/nvim-autopairs',
-    'hrsh7th/cmp-nvim-lsp',
-    'onsails/lspkind-nvim',
-  },
-  opts = function()
-    local cmp = require('cmp')
-    local luasnip = require('luasnip')
-    local lspkind = require('lspkind')
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"windwp/nvim-autopairs",
+		"hrsh7th/cmp-nvim-lsp",
+		"onsails/lspkind-nvim",
+	},
+	opts = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		local lspkind = require("lspkind")
 
-    local function formatForTailwindCSS(entry, vim_item)
-      if vim_item.kind == 'Color' and entry.completion_item.documentation then
-        local _, _, r, g, b = string.find(entry.completion_item.documentation, '^rgb%((%d+), (%d+), (%d+)')
-        if r then
-          local color = string.format('%02x', r) .. string.format('%02x', g) .. string.format('%02x', b)
-          local group = 'Tw_' .. color
-          if vim.fn.hlID(group) < 1 then
-            vim.api.nvim_set_hl(0, group, { fg = '#' .. color })
-          end
-          vim_item.kind = "●"
-          vim_item.kind_hl_group = group
-          return vim_item
-        end
-      end
-      vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
-      return vim_item
-    end
+		local function formatForTailwindCSS(entry, vim_item)
+			if vim_item.kind == "Color" and entry.completion_item.documentation then
+				local _, _, r, g, b = string.find(entry.completion_item.documentation, "^rgb%((%d+), (%d+), (%d+)")
+				if r then
+					local color = string.format("%02x", r) .. string.format("%02x", g) .. string.format("%02x", b)
+					local group = "Tw_" .. color
+					if vim.fn.hlID(group) < 1 then
+						vim.api.nvim_set_hl(0, group, { fg = "#" .. color })
+					end
+					vim_item.kind = "●"
+					vim_item.kind_hl_group = group
+					return vim_item
+				end
+			end
+			vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
+			return vim_item
+		end
 
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
 
-    return {
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete({}),
-        ['<CR>'] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = false,
-        }),
-        ['<C-n>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-        ['<C-p>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-      }),
-      sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-      },
-      formatting = {
-        format = lspkind.cmp_format({
-          mode = 'symbol_text',
-          maxwidth = 50,
-          before = function(entry, vim_item)
-            vim_item = formatForTailwindCSS(entry, vim_item)
-            return vim_item
-          end
-        }),
-      }
-    }
-  end,
-  config = function(_, opts)
-    local cmp = require('cmp')
+		return {
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete({}),
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = false,
+				}),
+				["<C-n>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<C-p>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+			},
+			formatting = {
+				format = lspkind.cmp_format({
+					mode = "symbol_text",
+					maxwidth = 50,
+					before = function(entry, vim_item)
+						vim_item = formatForTailwindCSS(entry, vim_item)
+						return vim_item
+					end,
+				}),
+			},
+		}
+	end,
+	config = function(_, opts)
+		local cmp = require("cmp")
 
-    cmp.setup(opts)
-  end,
+		cmp.setup(opts)
+	end,
 }
 
 return M
