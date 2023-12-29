084640857fc937239003c817693248f51e5e1581
Cristhian Melo <rxtsel@hotmail.com>
2023-12-29 17:55:41 -0500

    chore: remove unused plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,7 +34,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    -- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "plugins" },
   },
   defaults = {
