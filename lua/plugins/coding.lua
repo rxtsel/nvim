11167b6d61faefd13d1e613cc6c6c09e40608718
Cristhian Melo <rxtsel@hotmail.com>
2023-12-29 17:52:58 -0500

    chore: remove unused plugin

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -79,15 +79,6 @@ return {
   },
 
   {
-    "simrat39/symbols-outline.nvim",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-    cmd = "SymbolsOutline",
-    opts = {
-      position = "right",
-    },
-  },
-
-  {
     "nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
     opts = function(_, opts)
