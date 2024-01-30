a799e58aa20004d144903ca273c7958fcafea359
Cristhian Melo <rxtsel@hotmail.com>
2024-01-30 12:28:27 -0500

    fix(editor): highlighting hsl colors not working in CSS

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -19,11 +19,13 @@ return {
     opts = {
       highlighters = {
         hsl_color = {
-          pattern = "hsl%(%d+,? %d+,? %d+%)",
+          pattern = "hsl%(%d+,? %d+%%?,? %d+%%?%)",
           group = function(_, match)
             local utils = require("solarized-osaka.hsl")
-            local h, s, l = match:match("hsl%((%d+),? (%d+),? (%d+)%)")
-            h, s, l = tonumber(h), tonumber(s), tonumber(l)
+            --- @type string, string, string
+            local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%?,? (%d+)%%?%)")
+            --- @type number?, number?, number?
+            h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
             local hex_color = utils.hslToHex(h, s, l)
             return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
           end,
