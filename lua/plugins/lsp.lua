6d33dfbafe93041317f00a36a9e047b2e93a3b87
Cristhian Melo <rxtsel@hotmail.com>
2024-03-05 14:38:54 -0500

    fix(lsp): disable `reuse_win` option in telescope

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,6 +25,18 @@ return {
   -- lsp servers
   {
     "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = {
+        "gd",
+        function()
+          -- DO NOT RESUSE WINDOW
+          require("telescope.builtin").lsp_definitions({ reuse_win = false })
+        end,
+        desc = "Goto Definition",
+        has = "definition",
+      }
+    end,
     opts = {
       inlay_hints = { enabled = true },
       ---@type lspconfig.options
