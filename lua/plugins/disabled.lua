6e263a282ae5e0d9637b1c3b2c2c3021449bc003
Cristhian Melo <rxtsel@hotmail.com>
2024-05-22 20:08:49 -0500

    chore(plugins): restore refactoring

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,7 +3,6 @@ return {
   { "catppuccin", enabled = false },
   { "nonaqa/dial.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
-  { "refactoring.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
 }
