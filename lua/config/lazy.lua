7d36d24ff4b60bbf390952e793c21aa50e598ab1
Cristhian Melo <rxtsel@hotmail.com>
2024-10-23 22:25:58 -0500

    chore: update config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,9 @@ require("lazy").setup({
     {
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
-      opts = { colorscheme = "solarized-osaka" },
+      opts = {
+        colorscheme = "solarized-osaka",
+      },
     },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.copilot" },
@@ -48,7 +50,7 @@ require("lazy").setup({
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = true,
+    lazy = false,
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,18 +20,6 @@ return {
   -- lsp servers
   {
     "neovim/nvim-lspconfig",
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = {
-        "gd",
-        function()
-          -- DO NOT RESUSE WINDOW
-          require("telescope.builtin").lsp_definitions({ reuse_win = false })
-        end,
-        desc = "Goto Definition",
-        has = "definition",
-      }
-    end,
     opts = {
       inlay_hints = { enabled = false },
 
@@ -150,5 +138,22 @@ return {
       },
       setup = {},
     },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      vim.list_extend(keys, {
+        {
+          "gd",
+          function()
+            -- DO NOT RESUSE WINDOW
+            require("telescope.builtin").lsp_definitions({ reuse_win = false })
+          end,
+          desc = "Goto Definition",
+          has = "definition",
+        },
+      })
+    end,
   },
 }
