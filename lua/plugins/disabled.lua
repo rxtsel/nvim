a8f81e60bed1e0836614d60e058be6c02f14158c
Cristhian Melo <rxtsel@hotmail.com>
2024-10-23 23:54:32 -0500

    chore: enable flash

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,6 +3,5 @@ return {
   { "nonaqa/dial.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false },
   { "echasnovski/mini.hipatterns", enabled = false },
 }
