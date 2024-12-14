8dc37ce03ec5c295d012d6bf60760e6e9dd198a4
Cristhian Melo <rxtsel@hotmail.com>
2024-12-14 18:49:10 -0500

    chore(plugins): remove mini animate

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -75,17 +75,6 @@ return {
     },
   },
 
-  -- animations
-  {
-    "echasnovski/mini.animate",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      opts.scroll = {
-        enable = false,
-      }
-    end,
-  },
-
   -- buffer line
   {
     "akinsho/bufferline.nvim",
