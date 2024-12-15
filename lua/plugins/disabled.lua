58b49e46b6c31546acbecea672bb1f60dc6ddee8
Cristhian Melo <rxtsel@hotmail.com>
2024-12-14 21:28:58 -0500

    chore: remove mini hipatterns from disabled plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,5 +3,4 @@ return {
   { "nonaqa/dial.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
-  { "echasnovski/mini.hipatterns", enabled = false },
 }
