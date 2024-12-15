82c94cc1b38769967c3847158cc953f5aff0f865
Cristhian Melo <rxtsel@hotmail.com>
2024-12-15 09:43:34 -0500

    chore(plugins): add new plugins and remove others

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "catppuccin", enabled = false },
-  { "nonaqa/dial.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
 }
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,26 +1,5 @@
 return {
   {
-    "echasnovski/mini.hipatterns",
-    event = "BufReadPre",
-    opts = {
-      highlighters = {
-        hsl_color = {
-          pattern = "hsl%(%d+,? %d+%%?,? %d+%%?%)",
-          group = function(_, match)
-            local utils = require("solarized-osaka.hsl")
-            --- @type string, string, string
-            local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%?,? (%d+)%%?%)")
-            --- @type number?, number?, number?
-            h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
-            local hex_color = utils.hslToHex(h, s, l)
-            return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
-          end,
-        },
-      },
-    },
-  },
-
-  {
     "dinhhuy258/git.nvim",
     event = "BufReadPre",
     opts = {
