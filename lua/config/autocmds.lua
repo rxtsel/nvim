e9539f629222625017e6ab3d24681d80a81b3e3c
Cristhian Melo <rxtsel@hotmail.com>
2024-12-15 10:24:26 -0500

    chore(indent): change indent colors

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,6 +4,10 @@ vim.api.nvim_create_autocmd("InsertLeave", {
   command = "set nopaste",
 })
 
+-- Indent colors
+vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#002B36", bg = "NONE", nocombine = true })
+vim.api.nvim_set_hl(0, "SnacksIndentScope", { fg = "#494FB6", bg = "NONE", nocombine = true })
+
 -- Disable the concealing in some file formats
 -- The default conceallevel is 3 in LazyVim
 vim.api.nvim_create_autocmd("FileType", {
