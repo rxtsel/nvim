bd995bd501541d68a63d8f92218f59e4861ea5d3
Cristhian Melo <rxtsel@hotmail.com>
2024-12-15 10:38:24 -0500

    chore(keymaps): update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,20 +3,29 @@ local wk = require("which-key")
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
+-- Do things without affecting the registers
 keymap.set("n", "x", '"_x')
+keymap.set("n", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>P", '"0P')
+keymap.set("v", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>c", '"_c')
+keymap.set("n", "<Leader>C", '"_C')
+keymap.set("v", "<Leader>c", '"_c')
+keymap.set("v", "<Leader>C", '"_C')
+keymap.set("n", "<Leader>d", '"_d')
+keymap.set("n", "<Leader>D", '"_D')
+keymap.set("v", "<Leader>d", '"_d')
+keymap.set("v", "<Leader>D", '"_D')
 
 -- Increment/decrement
 keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x")
+keymap.set("n", "-", "<C-x>")
 
 -- Delete a word backwardsk
 keymap.set("n", "dw", "vb_d")
 
 -- Select all
-keymap.set("n", "<leader><C-a>", "gg<S-v>G")
-
--- Jumplist
-keymap.set("n", "<C-m>", "<C-i>", opts)
+keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- New tab
 keymap.set("n", "te", "<cmd>tabedit<Return>", opts)
