e53b6a2fd4b4f853746157cecc09a2fe543371d1
Cristhian Melo <rxtsel@hotmail.com>
2025-03-07 12:24:28 -0500

    feat(plugins): add `kulala.nvim` minimal rest client

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -82,4 +82,19 @@ return {
       end,
     },
   },
+
+  -- Rest Client
+  {
+    "mistweaverco/kulala.nvim",
+    keys = {
+      { "<leader>Rs", desc = "Send request" },
+      { "<leader>Ra", desc = "Send all requests" },
+      { "<leader>Rb", desc = "Open scratchpad" },
+    },
+    ft = { "http", "rest" },
+    opts = {
+      -- your configuration comes here
+      global_keymaps = true,
+    },
+  },
 }
