f96e730fb364b4a97cd2f0d14f3a88d07dc368cb
Cristhian Melo <rxtsel@outlook.com>
2025-04-03 14:53:56 -0500

    feat(plugins): add screenkeys

---
+++ lua/plugins/screenkeys.lua
@@ -0,0 +1,11 @@
+return {
+  "NStefan002/screenkey.nvim",
+  lazy = false,
+  version = "*", -- or branch = "dev", to use the latest commit
+  opts = {
+    win_opts = {
+      height = 1,
+      col = 0,
+    },
+  },
+}
