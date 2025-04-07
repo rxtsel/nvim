623a3d1819cbbba11b7181d6055d8b824754a317
Cristhian Melo <rxtsel@outlook.com>
2025-04-07 11:12:24 -0500

    feat(formatting): enable prettier plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
     { import = "lazyvim.plugins.extras.editor.snacks_picker" },
     { import = "lazyvim.plugins.extras.editor.dial" },
-    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.formatting.biome" },
     { import = "lazyvim.plugins.extras.lang.astro" },
     { import = "lazyvim.plugins.extras.lang.json" },
