235a02e261db9820e12260373c0afd16f117c5c7
Cristhian Melo <rxtsel@outlook.com>
2025-05-22 04:41:12 -0500

    refactor(snacks): update explorer position and add autoclose

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          auto_close = true,
+          layout = { layout = { position = "right" } },
+        },
+      },
+    },
+  },
+}
