a0b93ad746a7691bef157c4c95a6e5d5fe00459d
Cristhian Melo <rxtsel@outlook.com>
2025-05-22 06:57:24 -0500

    feat(langs): add default langs lsp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,12 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.util.rest" },
+    { import = "lazyvim.plugins.extras.lang.astro" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
     -- import/override with your plugins
     { import = "plugins" },
   },
