c467c643c533ee03e388281363ca1cacf9ed3801
Cristhian Melo <rxtsel@outlook.com>
2025-05-22 23:24:17 -0500

    feat(autocmd): add base for autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,31 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- Show cursor line only in active window
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+	callback = function()
+		if vim.w.auto_cursorline then
+			vim.wo.cursorline = true
+			vim.w.auto_cursorline = nil
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+	callback = function()
+		if vim.wo.cursorline then
+			vim.w.auto_cursorline = true
+			vim.wo.cursorline = false
+		end
+	end,
+})
+
+-- Backups
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = vim.api.nvim_create_augroup("better_backup", { clear = true }),
+	callback = function(event)
+		local file = vim.uv.fs_realpath(event.match) or event.match
+		local backup = vim.fn.fnamemodify(file, ":p:~:h")
+		backup = backup:gsub("[/\\]", "%%")
+		vim.go.backupext = backup
+	end,
+})
+
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
