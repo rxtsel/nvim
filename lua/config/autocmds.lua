e947710aa79f802da23c9e2fbb747f3d65ce3942
Cristhian Melo <rxtsel@outlook.com>
2025-08-09 14:01:47 -0500

    rollback(autocmd): restore custom options

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,5 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+	pattern = "*",
+	command = "set nopaste",
+})
