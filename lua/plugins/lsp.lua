bd21f8e7f0999132b75b315ad1ab2b19401a0046
Cristhian Melo <rxtsel@outlook.com>
2025-08-09 14:08:28 -0500

    chore(lsp): disable inline hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,6 +2,25 @@ return {
 	"neovim/nvim-lspconfig",
 	---@class PluginLspOpts
 	opts = {
+		-- options for vim.diagnostic.config()
 		inlay_hints = { enabled = false },
+		---@type vim.diagnostic.Opts
+		-- @type lspconfig.options
+		servers = {
+			vtsls = {
+				settings = {
+					typescript = {
+						inlayHints = {
+							enumMemberValues = { enabled = false },
+							functionLikeReturnTypes = { enabled = false },
+							parameterNames = { enabled = false },
+							parameterTypes = { enabled = false },
+							propertyDeclarationTypes = { enabled = false },
+							variableTypes = { enabled = false },
+						},
+					},
+				},
+			},
+		},
 	},
 }
