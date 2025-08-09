e046c88709c87c649f9b5630f8a793132efeb01d
Cristhian Melo <rxtsel@outlook.com>
2025-08-09 14:00:45 -0500

    rollback(keymaps): restore keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,36 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 local wk = require("which-key")
 
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwardsk
+keymap.set("n", "dw", "vb_d")
+
+-- New tab
+keymap.set("n", "te", "<cmd>tabedit<Return>", opts)
+keymap.set("n", "<tab>", "<cmd>tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", "<cmd>tabprev<Return>", opts)
+
+-- Split window
+keymap.set("n", "ss", "<cmd>split<Return>", opts)
+keymap.set("n", "sv", "<cmd>vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- which-key
 wk.add({
 	{ "<leader>O", group = "Obsidian" },
 	{ "<leader>a", group = "ai" },
