1feb28f142255d56f8bd9e66ad4ccf9b0dbea665
Cristhian Melo <rxtsel@outlook.com>
2025-08-21 15:40:17 -0500

    fix(kulala): disable autoformat for kualala

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,3 +3,11 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 	pattern = "*",
 	command = "set nopaste",
 })
+
+-- Disable autoformat for http/rest files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "http", "rest" },
+	callback = function()
+		vim.b.autoformat = false
+	end,
+})
