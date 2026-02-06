b5efef7225def1c1c2a6f7880401b5fde4d28c0f
Cristhian Melo <rxtsel@outlook.com>
2026-02-06 10:27:50 -0500

    refactor(oil): ignore files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,4 +1,19 @@
+local IGNORED_FILES = {
+	["node_modules"] = true,
+	[".git"] = true,
+	[".DS_Store"] = true,
+	[".opencode"] = true,
+	[".svelte-kit"] = true,
+	[".vscode"] = true,
+	[".direnv"] = true,
+}
+
 require("oil").setup({
 	columns = { "icon" },
-	view_options = { show_hidden = true }
+	view_options = {
+		show_hidden = true,
+		is_always_hidden = function(name, _)
+			return IGNORED_FILES[name] == true
+		end,
+	},
 })
