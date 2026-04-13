ccfcb42407e435a221c0bf33cd802d23feb45ab6
Cristhian Melo <rxtsel@outlook.com>
2026-04-13 04:21:11 -0500

    feat(lsp): enable support for copilot lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -23,4 +23,32 @@ vim.lsp.config("lua_ls", {
 	},
 })
 
+-- Force virtual style for color highlight
 vim.lsp.document_color.enable(true, nil, { style = "virtual" })
+
+-- Enable inline completion for copilot lsp
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		local bufnr = args.buf
+
+		if client.name == "copilot" then
+			if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlineCompletion, bufnr) then
+				vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
+
+				vim.keymap.set(
+					"i",
+					"<A-l>",
+					vim.lsp.inline_completion.get,
+					{ buffer = bufnr, desc = "Trigger inline completion" }
+				)
+				vim.keymap.set(
+					"i",
+					"<A-]>",
+					vim.lsp.inline_completion.select,
+					{ buffer = bufnr, desc = "Select next inline completion" }
+				)
+			end
+		end
+	end,
+})
