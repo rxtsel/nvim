58e11e1f7d652e6852e73cbf8c093328a81848e2
Cristhian Melo <rxtsel@outlook.com>
2026-04-13 02:33:27 -0500

    feat(autocmd): add new autocmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -4,12 +4,23 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
 		vim.hl.on_yank({ timeout = 170 })
 	end,
-	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
 })
 
 -- Enable automatic treesitter highlighting using Neovim's native API
 vim.api.nvim_create_autocmd("FileType", {
 	callback = function(args)
 		pcall(vim.treesitter.start, args.buf)
-	end
+	end,
+})
+
+-- Wrap,linebreak and spellcheck on markdown and text files
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("BetterReadForTextFiles", { clear = true }),
+	pattern = { "markdown", "text", "gitcommit" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.linebreak = true
+		vim.opt_local.spell = true
+	end,
 })
