32724e7764ab9eb5b1d796401a1b77c8436214b6
Cristhian Melo <rxtsel@outlook.com>
2026-04-13 02:57:32 -0500

    refactor(keymaps): delete util fn and add more keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,45 +1,51 @@
-local opts = { noremap = true, silent = true }
-
--- Builds a keymap options table with the given description
-local function with_desc(desc)
-	return vim.tbl_extend("force", {}, opts, { desc = desc })
-end
-
--- Save and exit
-vim.keymap.set("n", "<C-s>", "<CMD>w<CR>", with_desc("Save file"))
-vim.keymap.set("n", "<leader>q", "<CMD>q<CR>", with_desc("Quit neovim"))
+-- Save / quit
+vim.keymap.set("n", "<C-s>", "<CMD>w<CR>", { desc = "Save file", silent = true })
+vim.keymap.set("n", "<leader>q", "<CMD>q<CR>", { desc = "Quit neovim", silent = true })
 
 -- File explorer
-vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", with_desc("Toggle file explorer"))
-vim.keymap.set("n", ";f", "<CMD>Pick files<CR>", with_desc("Find files"))
-vim.keymap.set("n", ";r", "<CMD>Pick grep<CR>", with_desc("Find grep"))
-vim.keymap.set("n", ";b", "<CMD>Pick buffers<CR>", with_desc("Find buffers"))
-vim.keymap.set("n", ";h", "<CMD>Pick help<CR>", with_desc("Find help"))
+vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Toggle file explorer", silent = true })
+vim.keymap.set("n", ";f", "<CMD>Pick files<CR>", { desc = "Find files", silent = true })
+vim.keymap.set("n", ";r", "<CMD>Pick grep<CR>", { desc = "Find grep", silent = true })
+vim.keymap.set("n", ";b", "<CMD>Pick buffers<CR>", { desc = "Find buffers", silent = true })
+vim.keymap.set("n", ";h", "<CMD>Pick help<CR>", { desc = "Find help", silent = true })
 
 -- Format
-vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, with_desc("Format with lsp"))
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "Format with LSP", silent = true })
 
 -- Tabs
-vim.keymap.set("n", "te", "<CMD>tabnew<CR>", with_desc("New tab"))
-vim.keymap.set("n", "<S-l>", "<CMD>tabnext<CR>", with_desc("Next tab"))
-vim.keymap.set("n", "<S-h>", "<CMD>tabprev<CR>", with_desc("Previous tab"))
+vim.keymap.set("n", "te", "<CMD>tabnew<CR>", { desc = "New tab", silent = true })
+vim.keymap.set("n", "<S-l>", "<CMD>tabnext<CR>", { desc = "Next tab", silent = true })
+vim.keymap.set("n", "<S-h>", "<CMD>tabprev<CR>", { desc = "Previous tab", silent = true })
 
--- Split window
-vim.keymap.set("n", "ss", "<CMD>split<CR>", with_desc("Split horizontal"))
-vim.keymap.set("n", "sv", "<CMD>vsplit<CR>", with_desc("Split vertical"))
+-- Splits
+vim.keymap.set("n", "ss", "<CMD>split<CR>", { desc = "Split horizontal", silent = true })
+vim.keymap.set("n", "sv", "<CMD>vsplit<CR>", { desc = "Split vertical", silent = true })
 
--- Move into windows
-vim.keymap.set("n", "sl", "<C-w>l", { desc = "Move to right" })
-vim.keymap.set("n", "sh", "<C-w>h", { desc = "Move to left" })
-vim.keymap.set("n", "sk", "<C-w>k", { desc = "Move to top" })
-vim.keymap.set("n", "sj", "<C-w>j", { desc = "Move to bottom" })
+-- Window navigation
+vim.keymap.set("n", "sl", "<C-w>l", { desc = "Move right", silent = true })
+vim.keymap.set("n", "sh", "<C-w>h", { desc = "Move left", silent = true })
+vim.keymap.set("n", "sk", "<C-w>k", { desc = "Move up", silent = true })
+vim.keymap.set("n", "sj", "<C-w>j", { desc = "Move down", silent = true })
 
--- Clear highlighting
-vim.keymap.set("n", "<Esc>", "<CMD>nohlsearch<CR>", { desc = "Move to bottom" })
+-- Clear highlight
+vim.keymap.set("n", "<Esc>", "<CMD>nohlsearch<CR>", { desc = "Clear search highlight", silent = true })
 
--- Better indentation in visual mode
-vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
-vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+-- Visual indent
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect", silent = true })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect", silent = true })
 
 -- Lazygit
-vim.keymap.set("n", "<leader>gg", "<CMD>LazyGit<CR>", { desc = "Toggle LazyGit" })
+vim.keymap.set("n", "<leader>gg", "<CMD>LazyGit<CR>", { desc = "Toggle LazyGit", silent = true })
+
+-- Move lines
+vim.keymap.set("n", "<A-k>", "<CMD>m .-2<CR>==", { desc = "Move line up", silent = true })
+vim.keymap.set("n", "<A-j>", "<CMD>m .+1<CR>==", { desc = "Move line down", silent = true })
+vim.keymap.set("v", "<A-k>", "<CMD>m '<-2<CR>gv=gv", { desc = "Move selection up", silent = true })
+vim.keymap.set("v", "<A-j>", "<CMD>m '>+1<CR>gv=gv", { desc = "Move selection down", silent = true })
+
+-- Utils
+vim.keymap.set("n", "<leader>pa", function()
+	local path = vim.fn.expand("%:p")
+	vim.fn.setreg("+", path)
+	print("file:", path)
+end, { desc = "Copy full file path" })
