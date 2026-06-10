25ee3e0696cf5698e1dbca931e6776f6a248e1a5
Cristhian Melo <rxtsel@outlook.com>
2026-06-10 18:35:29 -0500

    chore(pkgs): remove blink cursor

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -110,13 +110,3 @@ vim.opt.diffopt:append("algorithm:histogram")
 -- Wildmenu
 vim.opt.wildmenu = true
 vim.opt.wildmode = "longest:full,full"
-
--- Cursor
-vim.opt.guicursor = {
-	"n-v-c:block",
-	"i-ci-ve:ver25",
-	"r-cr:hor20",
-	"o:hor50",
-	"a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
-	"sm:block-blinkwait175-blinkoff150-blinkon175",
-}
