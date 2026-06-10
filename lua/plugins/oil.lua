422d4e26b8393dddaa1528e17fef1b5367514ddf
Cristhian Melo <rxtsel@outlook.com>
2026-06-10 18:08:30 -0500

    fix(oil): fix oil follow symlinks

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,1 +1,25 @@
-require("oil").setup()
+local function select_preserving_file_symlink()
+	local oil = require("oil")
+	local entry = oil.get_cursor_entry()
+	local dir = oil.get_current_dir()
+
+	if entry and dir and entry.type == "link" then
+		local path = dir .. entry.name
+
+		if vim.fn.isdirectory(path) == 0 then
+			vim.cmd.edit({ args = { path } })
+			return
+		end
+	end
+
+	oil.select()
+end
+
+require("oil").setup({
+	keymaps = {
+		["<CR>"] = {
+			callback = select_preserving_file_symlink,
+			desc = "Open entry without resolving file symlinks",
+		},
+	},
+})
