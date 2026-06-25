84f0e24466c2d204978da135a2bd982f28e96681
Cristhian Melo <rxtsel@outlook.com>
2026-06-24 22:24:23 -0500

    chore(theme): change color to fold icons

--- plugin/solarized-osaka.lua
+++ plugin/solarized-osaka.lua
@@ -1,11 +1,16 @@
 local add = require('vim-pack').add
 
-add({
+add {
     {
         src = 'craftzdog/solarized-osaka.nvim',
         setup = false,
         on_setup = function()
-            vim.cmd.colorscheme('solarized-osaka')
+            vim.cmd.colorscheme 'solarized-osaka'
+
+            -- Change color to fold icons
+            vim.api.nvim_set_hl(0, 'FoldColumn', {
+                link = 'Comment',
+            })
         end,
     },
-})
+}
